c7c0a4166848d0a5cf3c443e1a356b623b0f5105
laraimgabriel-pixel <laraimgabriel@gmail.com>
2026-09-10 23:17:54 +0800

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://gblrvlheofmpfhwcdtza.supabase.co";
-
-const SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_5JBPZj8aQAOD6CkS40aQQQ_Reg1294g";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
